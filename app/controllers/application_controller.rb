cd87d7af0df0abcffd8f073c0a1606c1af25ff0f
IsmaelRuby <ismael@palmatellez.cl>
2024-09-28 02:04:01 -0300

    busqueda

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+    Pagy::DEFAULT[:limit] = 10
+    include Pagy::Backend
 end
